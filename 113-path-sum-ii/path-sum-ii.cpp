3c219cb89a3797d9b2402585167dc8fc5921a460
Priyansh Khare <priyanshkhare2005@gmail.com>
2026-08-18 10:08:29 +0530

    Time: 0 ms (100.00%) | Memory: 20.8 MB (89.62%) - LeetSync

--- 113-path-sum-ii/path-sum-ii.cpp
+++ 113-path-sum-ii/path-sum-ii.cpp
@@ -13,11 +13,10 @@ class Solution {
 public:
     void dfs(TreeNode* root, int target, vector<vector<int>> & ans, vector<int>& temp){
         if(!root) return;
-        target -= root->val;
         temp.push_back(root->val);
-        if(!root->left && !root->right && target == 0) ans.push_back(temp);
-        if(root->left) dfs(root->left, target, ans, temp);
-        if(root->right) dfs(root->right, target, ans, temp);
+        if(!root->left && !root->right && target == root->val) ans.push_back(temp);
+        if(root->left) dfs(root->left, target - root->val, ans, temp);
+        if(root->right) dfs(root->right, target - root->val, ans, temp);
         temp.pop_back();
     }
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
