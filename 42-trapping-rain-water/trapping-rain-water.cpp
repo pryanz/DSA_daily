1197dadc3b72297e5bc31326008886f942fba0f6
Priyansh Khare <priyanshkhare2005@gmail.com>
2026-09-03 11:01:45 +0530

    Time: 0 ms (100.00%) | Memory: 26 MB (56.08%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     int trap(vector<int>& arr) {
-        int n = arr.size();
-        vector<int> leftMax(n);
-        vector<int> rightMax(n);
-        leftMax[0] = arr[0];
-        rightMax[n-1] = arr[n-1];
-        for(int i = 1; i < n; i++){
-            leftMax[i] = max(leftMax[i-1],arr[i]);
-            rightMax[n-1-i] = max(rightMax[n-i],arr[n-1-i]);
-        }
-
+        int i = 0; int j = arr.size() - 1;
+        int l_max = arr[i]; int r_max = arr[j];
         int total = 0;
-        for(int i = 1; i < n-1; i++){
-            if(arr[i] < leftMax[i - 1] && arr[i] < rightMax[i + 1]){
-                total += min(leftMax[i-1] , rightMax[i + 1]) - arr[i];
+
+        while(i < j){
+            if(l_max <= r_max){
+                total += l_max - arr[i];
+                i++;
+                l_max = max(l_max, arr[i]);
+            } else {
+                total += r_max - arr[j];
+                j--;
+                r_max = max(r_max, arr[j]);
             }
         }
         return total;
