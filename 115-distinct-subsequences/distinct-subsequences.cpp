5e2b85d9874f2bec516ca40589238fb47965d1ae
Priyansh Khare <priyanshkhare2005@gmail.com>
2026-09-06 11:23:26 +0530

    Time: 15 ms (82.67%) | Memory: 44.3 MB (31.09%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     vector<vector<long long>> dp;
-    int solve(string & s, string &t, int i , int j){
+    long long solve(string & s, string &t, int i , int j){
         if(j >= t.size()){
             return 1;
         }
         if(i >= s.size()) return 0;
+        if(s.size() - i < t.size() - j) return 0;
 
         if(dp[i][j] != -1) return dp[i][j];
 
@@ -18,7 +19,7 @@ public:
 
     int numDistinct(string s, string t) {
         dp.assign(s.size(), vector<long long>(t.size(),-1));
-        return solve(s, t, 0 , 0);
+        return (int)solve(s, t, 0 , 0);
 
     }
 };
